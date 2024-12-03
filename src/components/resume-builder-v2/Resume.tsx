d449dc07538f3f0975464186e97ef7bb1f70c63a
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-03 22:33:58 +0530

    chore(Resume): added font Montserrat to the component, Title and SocialHandles

--- src/components/resume-builder-v2/Resume.tsx
+++ src/components/resume-builder-v2/Resume.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Montserrat } from 'next/font/google';
 
-// HOOKS
-import { useResumeTheme } from './hooks/use-resume-theme';
+// COMPONENTS
+import { Title } from './components/Title';
+import { SocialHandles } from './components/SocialHandles';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+});
+
+const A4_SHEET_CONFIG = {
+  width: '595px',
+  height: '842px',
+};
 
 export const Resume = () => {
-  const { color } = useResumeTheme();
-  return <div style={{ color }}>Resume</div>;
+  return (
+    <div className={`${montserrat.className} shadow-md w-3/4 bg-slate-50 mt-2`}>
+      {/** Title */}
+      <Title />
+      {/** Social Handles */}
+      <SocialHandles />
+    </div>
+  );
 };
