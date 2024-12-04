e1e6bbafba979ab28c4418cca187fbbeaf2ee738
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-04 22:35:40 +0530

    chore(types/layout.ts): added types for layout context.

---
+++ src/components/resume-builder-v2/types/layout.ts
@@ -0,0 +1,11 @@
+export type DisplayMode = 'edit' | 'preview';
+
+export type ActiveSectionName =
+  | ''
+  | 'title'
+  | 'socialHandles'
+  | 'education'
+  | 'workExperience'
+  | 'projects'
+  | 'achievements'
+  | 'skills';
