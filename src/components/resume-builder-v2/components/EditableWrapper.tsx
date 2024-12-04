6bb1b6f231a06febd5b29122ff5ccb9cb2d1e0e0
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-04 22:37:32 +0530

    chore(EditableWrapper.tsx): added wrapper component to highlight a resume section on hover for editing.

---
+++ src/components/resume-builder-v2/components/EditableWrapper.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { MdOutlineEdit } from 'react-icons/md';
+import { ActiveSectionName } from '../types/layout';
+import { useLayout } from '../context/LayoutContext';
+
+export const EditableWrapper: React.FC<{
+  id: ActiveSectionName;
+  children: React.ReactNode;
+}> = ({ id, children }) => {
+  const { toggleDisplayMode } = useLayout();
+  const onClickHandler = () => {
+    toggleDisplayMode(id);
+  };
+
+  return (
+    <div
+      onClick={onClickHandler}
+      className="relative border border-transparent hover:border-blue-300 group transition duration-300 ease-in-out cursor-pointer"
+    >
+      {/* Edit Icon */}
+      <div className="absolute top-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+        <MdOutlineEdit className="text-blue-300" />
+      </div>
+      {/* Content */}
+      {children}
+    </div>
+  );
+};
