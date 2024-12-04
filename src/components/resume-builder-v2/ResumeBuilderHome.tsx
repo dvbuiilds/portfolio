64241eb19df7e176df7dac63559516d2520f5c22
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-04 22:39:28 +0530

    chore(ResumeBuilderHome.tsx): made ResumeBuilderHome as HOC to ResumeBuilder that will change the layout for edit and preview mode.

--- src/components/resume-builder-v2/ResumeBuilderHome.tsx
+++ src/components/resume-builder-v2/ResumeBuilderHome.tsx
@@ -1,19 +1,45 @@
 'use client';
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
+
+// PROVIDERS
+import { ResumeDataProvider } from './context/ResumeDataContext';
+import { LayoutProvider, useLayout } from './context/LayoutContext';
 
 // COMPONENTS
 import { ResumeThemeProvider } from './context/ResumeThemeContext';
 import { Resume } from './Resume';
 import { ThemeChangingNavbar } from './ThemeChangingNavbar';
-import { ResumeDataProvider } from './context/ResumeDataContext';
 
+// This Component is an HOC for ResumeBuilder so that the later can access LayoutContext.
 export const ResumeBuilderHome = () => {
   return (
     <ResumeThemeProvider>
-      <ThemeChangingNavbar />
-      <ResumeDataProvider>
-        <Resume />
-      </ResumeDataProvider>
+      <LayoutProvider>
+        <ResumeBuilder />
+      </LayoutProvider>
     </ResumeThemeProvider>
+  );
+};
+
+export const ResumeBuilder: React.FC = () => {
+  const { displayMode } = useLayout();
+  const resumeWidthClassName = useMemo<string>(
+    () => (displayMode === 'preview' ? 'w-3/4' : 'w-2/3'),
+    [displayMode],
+  );
+  const renderEditPanel = useCallback(() => {
+    return displayMode === 'edit' ? <div>Edit Panel Here!</div> : null;
+  }, [displayMode]);
+
+  return (
+    <div className="flex flex-row w-full items-center justify-center">
+      <div className={`flex flex-col ${resumeWidthClassName} items-center`}>
+        <ThemeChangingNavbar />
+        <ResumeDataProvider>
+          <Resume />
+        </ResumeDataProvider>
+      </div>
+      {renderEditPanel()}
+    </div>
   );
 };
