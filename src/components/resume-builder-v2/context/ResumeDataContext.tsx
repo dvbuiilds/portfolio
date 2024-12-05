7720ea7977d57602f4c2ed801244b89ddf91641b
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-05 21:37:33 +0530

    chore(ResumeDataContext): updated the names of state setter functions.

--- src/components/resume-builder-v2/context/ResumeDataContext.tsx
+++ src/components/resume-builder-v2/context/ResumeDataContext.tsx
@@ -4,9 +4,11 @@ import type { SocialHandle } from '../types/resume-data';
 
 interface ResumeDataContextType {
   title: string;
-  setTitle: React.Dispatch<React.SetStateAction<string>>;
+  updateTitle: React.Dispatch<React.SetStateAction<string>>;
   socialHandles: Array<SocialHandle>;
-  setSocialHandles: React.Dispatch<React.SetStateAction<Array<SocialHandle>>>;
+  updateSocialHandles: React.Dispatch<
+    React.SetStateAction<Array<SocialHandle>>
+  >;
 }
 
 const ResumeDataContext = createContext<ResumeDataContextType | undefined>(
@@ -20,15 +22,15 @@ export const ResumeDataProvider: React.FC<{ children: ReactNode }> = ({
    * States to save the Resume Data.
    * At the same time, I decided to keep the states seperate and not a consolidated object, because updating the state can be a tedious task for sections like Work Experience, etc that have big complex objects.
    */
-  const [title, setTitle] = useState<string>('Enter Your Name');
-  const [socialHandles, setSocialHandles] = useState<Array<SocialHandle>>([
+  const [title, updateTitle] = useState<string>('Enter Your Name');
+  const [socialHandles, updateSocialHandles] = useState<Array<SocialHandle>>([
     { label: 'abc@example.com', link: 'use mailto: in the link' },
     { label: 'LinkedIn', link: '#' },
   ]);
 
   return (
     <ResumeDataContext.Provider
-      value={{ title, setTitle, socialHandles, setSocialHandles }}
+      value={{ title, updateTitle, socialHandles, updateSocialHandles }}
     >
       {children}
     </ResumeDataContext.Provider>
