b55c764593e9a7c9f131f5e25e1cf57744c01439
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-07 23:34:33 +0530

    chore(./types/resume-data): added types for Work Experience section.

--- src/components/resume-builder-v2/types/resume-data.ts
+++ src/components/resume-builder-v2/types/resume-data.ts
@@ -2,3 +2,16 @@ export interface SocialHandle {
   label: string;
   link: string;
 }
+
+export interface Experience {
+  companyName: string;
+  jobTitle: string;
+  startDate: string;
+  endDate: string;
+  description: string[];
+}
+
+export interface WorkExperience {
+  title: string;
+  experience: Experience[];
+}
