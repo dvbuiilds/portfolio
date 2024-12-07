17e10674f40a7fd574fb9d53c25e5e1f213db8e2
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-07 23:37:23 +0530

    chore(ResumeDataContext): added intialResumeData object + workExperience state created & exposed in provider value.

--- src/components/resume-builder-v2/context/ResumeDataContext.tsx
+++ src/components/resume-builder-v2/context/ResumeDataContext.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { createContext, useContext, useState, ReactNode } from 'react';
-import type { SocialHandle } from '../types/resume-data';
+import type { SocialHandle, WorkExperience } from '../types/resume-data';
 
 interface ResumeDataContextType {
   title: string;
@@ -9,11 +9,37 @@ interface ResumeDataContextType {
   updateSocialHandles: React.Dispatch<
     React.SetStateAction<Array<SocialHandle>>
   >;
+  workExperience: WorkExperience;
+  updateWorkExperience: React.Dispatch<React.SetStateAction<WorkExperience>>;
 }
 
 const ResumeDataContext = createContext<ResumeDataContextType | undefined>(
   undefined,
 );
+
+const initialResumeData: {
+  title: string;
+  socialHandles: Array<SocialHandle>;
+  workExperience: WorkExperience;
+} = {
+  title: 'Enter Your Name',
+  socialHandles: [
+    { label: 'abc@example.com', link: 'use mailto: in the link' },
+    { label: 'LinkedIn', link: '#' },
+  ],
+  workExperience: {
+    title: 'WORK EXPERIENCE',
+    experience: [
+      {
+        companyName: 'Google Inc.',
+        jobTitle: 'Software Engineer',
+        startDate: 'June 2023',
+        endDate: 'Present',
+        description: ['Worked on YouTube Ads Scaling.'],
+      },
+    ],
+  },
+};
 
 export const ResumeDataProvider: React.FC<{ children: ReactNode }> = ({
   children,
@@ -22,15 +48,24 @@ export const ResumeDataProvider: React.FC<{ children: ReactNode }> = ({
    * States to save the Resume Data.
    * At the same time, I decided to keep the states seperate and not a consolidated object, because updating the state can be a tedious task for sections like Work Experience, etc that have big complex objects.
    */
-  const [title, updateTitle] = useState<string>('Enter Your Name');
-  const [socialHandles, updateSocialHandles] = useState<Array<SocialHandle>>([
-    { label: 'abc@example.com', link: 'use mailto: in the link' },
-    { label: 'LinkedIn', link: '#' },
-  ]);
+  const [title, updateTitle] = useState<string>(initialResumeData.title);
+  const [socialHandles, updateSocialHandles] = useState<Array<SocialHandle>>(
+    initialResumeData.socialHandles,
+  );
+  const [workExperience, updateWorkExperience] = useState<WorkExperience>(
+    initialResumeData.workExperience,
+  );
 
   return (
     <ResumeDataContext.Provider
-      value={{ title, updateTitle, socialHandles, updateSocialHandles }}
+      value={{
+        title,
+        updateTitle,
+        socialHandles,
+        updateSocialHandles,
+        workExperience,
+        updateWorkExperience,
+      }}
     >
       {children}
     </ResumeDataContext.Provider>
