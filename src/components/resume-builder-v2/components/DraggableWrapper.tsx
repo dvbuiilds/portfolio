2ecdc9def2b2feaf21105676e17f91b0a1f93111
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-07 12:48:05 +0530

    chore(DraggableWrapper): added optional className prop for container div.

--- src/components/resume-builder-v2/components/DraggableWrapper.tsx
+++ src/components/resume-builder-v2/components/DraggableWrapper.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { RiDraggable } from 'react-icons/ri';
 
 export const DraggableWrapper: React.FC<{
+  className?: string;
   children: React.ReactNode;
-}> = ({ children }) => {
+}> = ({ className, children }) => {
   return (
-    <div className="flex items-center p-1 gap-0 bg-white border rounded shadow-sm">
+    <div
+      className={`flex items-center p-1 gap-0 bg-white border rounded shadow-sm ${className}`}
+    >
       <div className="mr-1 text-gray-400 cursor-move">
         <RiDraggable size={20} />
       </div>
