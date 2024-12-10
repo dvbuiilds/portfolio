906646d08628b6bb83c02125f28239096c77e149
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-10 22:02:37 +0530

    chore(Projects.tsx): created component for Projects section on resume.

---
+++ src/components/resume-builder-v2/components/resume-preview/Projects.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+// HOOKS
+import { useResumeData } from '../../context/ResumeDataContext';
+import { useResumeTheme } from '../../context/ResumeThemeContext';
+
+// TYPES
+import type { Project } from '../../types/resume-data';
+
+export const Projects: React.FC = () => {
+  const { projects } = useResumeData();
+  const { color } = useResumeTheme();
+
+  return (
+    <div className="my-1">
+      <h2 className="font-medium text-sm" style={{ color }}>
+        {projects.title}
+      </h2>
+      <hr className="w-full my-1" style={{ borderColor: color }} />
+      {projects.projects.map((project, index) => (
+        <Project key={`projects_${index}`} index={index} data={project} />
+      ))}
+    </div>
+  );
+};
+
+const Project: React.FC<{ index: number; data: Project }> = ({
+  index,
+  data,
+}) => {
+  return (
+    <div key={index} className="mb-4">
+      <div className="flex justify-between">
+        <div className="font-medium text-xs">
+          {data.organizationName} - {data.projectTitle}
+        </div>
+        <div className="font-medium text-xs">
+          {data.startDate} — {data.endDate}
+        </div>
+      </div>
+      <ul className="list-disc pl-5 mt-1 text-xs">
+        {data.description.map((desc, descInd) => (
+          <li key={`project_${index}_description_${descInd}`}>{desc}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
