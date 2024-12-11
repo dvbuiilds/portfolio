f892a4eb955af4734b0be4d3316c4835bb5cc105
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-11 20:17:11 +0530

    chore(Education): added education section in Resume preview.

---
+++ src/components/resume-builder-v2/components/resume-preview/Education.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+// HOOKS
+import { useResumeData } from '../../context/ResumeDataContext';
+import { useResumeTheme } from '../../context/ResumeThemeContext';
+
+// TYPES
+import type { Course } from '../../types/resume-data';
+
+export const Education: React.FC = () => {
+  const { education } = useResumeData();
+  const { color } = useResumeTheme();
+
+  return (
+    <div className="my-1">
+      <h2 className="font-medium text-sm" style={{ color }}>
+        {education.title}
+      </h2>
+      <hr className="w-full my-1" style={{ borderColor: color }} />
+      {education.courses.map((course, index) => (
+        <Course key={`educationCourse_${index}`} index={index} data={course} />
+      ))}
+    </div>
+  );
+};
+
+const Course: React.FC<{ index: number; data: Course }> = ({ index, data }) => {
+  return (
+    <div className="mb-4">
+      {/* Row for institution name and dates */}
+      <div className="flex justify-between mt-1">
+        <div className="text-xs font-medium">{data.institutionName}</div>
+        <div className="text-xs">
+          {data.startDate} — {data.endDate}
+        </div>
+      </div>
+      {/* Row for courseName and scoreEarned */}
+      <div className="flex justify-between">
+        <div className="font-medium text-xs">{data.courseName}</div>
+        <div className="font-medium text-xs">{data.scoreEarned}</div>
+      </div>
+      {/* Description list */}
+      <ul className="list-disc pl-5 mt-1 text-xs">
+        <li>{data.description}</li>
+      </ul>
+    </div>
+  );
+};
