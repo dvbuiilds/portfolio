18b4ad1910b49157aba5aa2a4a6caa25c407bfac
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-14 18:51:24 +0530

    chore(Resume): added sectionsOrder based renderdin of sections + added draggable context around each section.

--- src/components/resume-builder-v2/components/resume-preview/Resume.tsx
+++ src/components/resume-builder-v2/components/resume-preview/Resume.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Montserrat } from 'next/font/google';
 
+// THIRD_PARTY
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  type DropResult,
+} from '@hello-pangea/dnd';
+
+// HOOKS
+import { useLayout } from '../../context/LayoutContext';
+
 // COMPONENTS
 import { Title } from './Title';
 import { SocialHandles } from './SocialHandles';
@@ -12,6 +23,12 @@ import { Activities } from './Activities';
 import { Skills } from './Skills';
 import { Achievements } from './Achievements';
 
+// TYPES
+import { ActiveSectionName } from '../../types/layout';
+
+// CONFIGS
+import { SectionNameMapping } from '../../config/section-name-config';
+
 const montserrat = Montserrat({
   subsets: ['latin'],
 });
@@ -21,43 +38,95 @@ const A4_SHEET_CONFIG = {
   height: '842px',
 };
 
+const renderSection = (sectionName: ActiveSectionName) => {
+  switch (sectionName) {
+    case SectionNameMapping.TITLE: {
+      return <Title />;
+    }
+    case SectionNameMapping.SOCIAL_HANDLES: {
+      return <SocialHandles />;
+    }
+    case SectionNameMapping.WORK_EXPERIENCE: {
+      return <WorkExperience />;
+    }
+    case SectionNameMapping.PROJECTS: {
+      return <Projects />;
+    }
+    case SectionNameMapping.EDUCATION: {
+      return <Education />;
+    }
+    case SectionNameMapping.ACTIVITIES: {
+      return <Activities />;
+    }
+    case SectionNameMapping.SKILLS: {
+      return <Skills />;
+    }
+    case SectionNameMapping.ACHIEVEMENTS: {
+      return <Achievements />;
+    }
+    default: {
+      return;
+    }
+  }
+};
+
 export const Resume = () => {
+  const { sectionsOrder, updateSectionsOrder } = useLayout();
+
+  const onDragEnd = (result: DropResult<string>) => {
+    if (!result.destination) return;
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+
+    const newItems = Array.from(sectionsOrder);
+    const [reorderedItem] = newItems.splice(result.source.index, 1);
+    newItems.splice(result.destination.index, 0, reorderedItem);
+
+    updateSectionsOrder(newItems);
+  };
+
+  const renderSections = () => {
+    return (
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="list">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {sectionsOrder.map((sectionName, sectionIndex) => (
+                <Draggable
+                  key={`socialHandeLabel_${sectionIndex}`}
+                  draggableId={`socialHandeLabel_${sectionIndex}`}
+                  index={sectionIndex}
+                >
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <EditableWrapper
+                        key={`${sectionName}_${sectionIndex}`}
+                        id={sectionName}
+                      >
+                        {renderSection(sectionName)}
+                      </EditableWrapper>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  };
+
   return (
     <div
-      className={`${montserrat.className} shadow-md w-3/4 bg-slate-50 mt-2 px-2 py-4 flex flex-col`}
+      className={`${montserrat.className} shadow-md w-3/4 bg-slate-50 mt-2 p-4 flex flex-col`}
     >
-      {/** Title */}
-      <EditableWrapper id={'title'}>
-        <Title />
-      </EditableWrapper>
-      {/** Social Handles */}
-      <EditableWrapper id={'socialHandles'}>
-        <SocialHandles />
-      </EditableWrapper>
-      {/** Work Experience */}
-      <EditableWrapper id={'workExperience'}>
-        <WorkExperience />
-      </EditableWrapper>
-      {/** Projects */}
-      <EditableWrapper id={'projects'}>
-        <Projects />
-      </EditableWrapper>
-      {/** Education */}
-      <EditableWrapper id={'education'}>
-        <Education />
-      </EditableWrapper>
-      {/** Activities */}
-      <EditableWrapper id={'activities'}>
-        <Activities />
-      </EditableWrapper>
-      {/** Skills */}
-      <EditableWrapper id={'skills'}>
-        <Skills />
-      </EditableWrapper>
-      {/** Achievements */}
-      <EditableWrapper id={'achievements'}>
-        <Achievements />
-      </EditableWrapper>
+      {renderSections()}
     </div>
   );
 };
