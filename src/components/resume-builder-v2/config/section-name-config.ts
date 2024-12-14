b134e459c9a918ed3fbc096149342b804bb05519
dvbuiilds <varshneydhairya4@gmail.com>
2024-12-14 18:46:47 +0530

    chore(section-name-config): added section name mappings.

---
+++ src/components/resume-builder-v2/config/section-name-config.ts
@@ -0,0 +1,23 @@
+export const SectionNameMapping = {
+  EMPTY: '',
+  TITLE: 'title',
+  SOCIAL_HANDLES: 'socialHandles',
+  EDUCATION: 'education',
+  WORK_EXPERIENCE: 'workExperience',
+  PROJECTS: 'projects',
+  ACHIEVEMENTS: 'achievements',
+  ACTIVITIES: 'activities',
+  SKILLS: 'skills',
+} as const;
+
+export const SectionIdTitleMapping = {
+  '': '',
+  title: 'Title',
+  socialHandles: 'Social Handles',
+  education: 'Education',
+  workExperience: 'Work Experience',
+  projects: 'Projects',
+  achievements: 'Achievements',
+  activities: 'Activities',
+  skills: 'Skills',
+} as const;
